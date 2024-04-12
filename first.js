fae299b59fe7618a56a61ab6b279068081159659
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-12 22:22:50 +0700

    learn about argv and process.argv

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
-const mission = 'learn';
+const mission = process.argv[2];
 
 if (mission === 'learn') {
     console.log(`Time to write Node code!`);
@@ -6,3 +6,6 @@ if (mission === 'learn') {
     //console.log('Is ' + mission + ' really more fun?');
     console.log(`Is ${mission} really more fun?`);
 }
+
+//[ 'C:\\Program Files\\nodejs\\node.exe' ]
+
